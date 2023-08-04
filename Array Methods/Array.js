289d01203450b6f7332e7aacb1e95a56fc961c17
Abhishek Bondre <abhibondre27@gmail.com>
2023-08-04 21:06:02 +0530

    array methods

---
+++ Array Methods/Array.js
@@ -0,0 +1,21 @@
+// Methods:-
+
+// To remove and add element at the end of array:- pop & push
+// To remove and add element at the front of array:- shift & unshift
+
+// push method -
+let array = [10, 20, 30, 40, 50];
+// array.push(60);
+// console.log(array); // [10,20,30,40,50,60]
+
+// pop method
+// array.pop();
+// console.log(array);
+
+//shift method
+// array.shift();  to remove to the first element to array
+// console.log(array);
+
+// unshift method
+array.unshift("abhi"); // to add to the first element to array
+console.log(array);
